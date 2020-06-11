6e043eaaadbdee5048a17ba282b59195f3e12519
Karen Ventura <akv802@gmail.com>
2020-06-11 10:13:06 -0500

    Add unit tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,18 @@
 defmodule CardsTest do
   use ExUnit.Case
-  doctest Cards
+  doctest Cards # with this line we said that we want to run docs tests
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+
+  test "Create a deck of 20 cards" do
+    deck_length =  length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck =  Cards.create_deck
+    refute deck == Cards.shuffle(deck)
   end
 end
